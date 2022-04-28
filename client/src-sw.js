6b32463f058d418d99259f775832afe18cf1f4ff
Roberto Gonzalez <rgonzalez7359@gmail.com>
2022-04-28 16:07:31 -0700

    added caching and required StaleWhileRevalidate

--- client/src-sw.js
+++ client/src-sw.js
@@ -4,6 +4,7 @@ const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
 const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
+const { StaleWhileRevalidate } = require('workbox-strategies');
 
 precacheAndRoute(self.__WB_MANIFEST);
 
@@ -28,13 +29,12 @@ registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
 registerRoute(
-  // Here we define the callback function that will filter the requests we want to cache (in this case, JS and CSS files)
   ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
   new StaleWhileRevalidate({
     // Name of the cache storage.
     cacheName: 'asset-cache',
     plugins: [
-      // This plugin will cache responses with these headers to a maximum-age of 30 days
+      // cache maximum-age of 30 days
       new CacheableResponsePlugin({
         statuses: [0, 200],
       }),
